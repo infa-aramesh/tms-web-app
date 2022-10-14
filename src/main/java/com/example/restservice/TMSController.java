4ee97b698ffc05c0c45c6b7a901efc0d9bd806b6
infa-aramesh <aramesh@infromatica.com>
2022-10-14 03:26:43 -0500

    update environment properties

--- src/main/java/com/example/restservice/TMSController.java
+++ src/main/java/com/example/restservice/TMSController.java
@@ -2,6 +2,7 @@ package com.example.restservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
