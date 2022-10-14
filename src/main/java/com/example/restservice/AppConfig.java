b5662ade42aaf3a111349686a095a29ba7f9ded2
infa-aramesh <aramesh@infromatica.com>
2022-10-14 04:48:52 -0500

    update environment properties

--- src/main/java/com/example/restservice/AppConfig.java
+++ src/main/java/com/example/restservice/AppConfig.java
@@ -3,8 +3,10 @@ package com.example.restservice;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class AppConfig {
 
     @Value("${t_val:45}")
