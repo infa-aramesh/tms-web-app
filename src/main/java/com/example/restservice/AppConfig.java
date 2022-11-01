f02e3686c8ccca5c398c4a38394f4fc811ece7f2
infa-aramesh <aramesh@infromatica.com>
2022-11-01 15:12:00 -0500

    refactor deploy folder

--- src/main/java/com/example/restservice/AppConfig.java
+++ src/main/java/com/example/restservice/AppConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 public class AppConfig {
 
     @Value("${test_val:10}")
